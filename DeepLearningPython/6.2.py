7d99bf99b8e49e09595cf4b27e58360d1c9bc81a
tavchija <it8816@student.uni-lj.si>
2021-11-18 15:21:44 +0100

    6.3

--- DeepLearningPython/6.2.py
+++ DeepLearningPython/6.2.py
@@ -6,7 +6,7 @@ samples = ['The cat sat on the mat.', 'The dog ate my homework']
 characters = string.printable
 
 token_index = dict(zip(characters, range(1, len(characters) + 1)))
-print(token_index)
+
 max_length = 50
 
 results = np.zeros((len(samples), max_length, 101))
