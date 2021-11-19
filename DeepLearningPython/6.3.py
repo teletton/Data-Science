3bf9c3a6e74e3e566aabfd7896affa1ea82a7335
iletavcioski <ilijatavchioski@gmail.com>
2021-11-19 21:31:34 +0100

    6.4

--- DeepLearningPython/6.3.py
+++ DeepLearningPython/6.3.py
@@ -4,7 +4,7 @@ samples = ['The cat sat on thne mat.', 'The dog ate my homework']
 
 tokenizer = Tokenizer(num_words = 1000)
 tokenizer.fit_on_texts(samples)
-   
+
 sequences = tokenizer.texts_to_sequences(samples)
 
 one_hot_results = tokenizer.texts_to_matrix(samples, mode='binary')
