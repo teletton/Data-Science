ea36448f4f2824c1e758faf1bcd1abce9f0d56e6
iletavcioski <ilijatavchioski@gmail.com>
2021-11-21 16:58:01 +0100

    6.6

--- DeepLearningPython/6.6.py
+++ DeepLearningPython/6.6.py
@@ -1,9 +1,11 @@
 from keras.datasets import imdb
 from keras import preprocessing
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Embedding
+import os
 
 if __name__ == "__main__":
+    os.environ['CUDA_VISIBLE_DEVICES'] = "0"
     max_features = 10000;
     maxlen = 20;
 
@@ -11,5 +13,17 @@ if __name__ == "__main__":
 
     x_train = preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
     x_test = preprocessing.sequence.pad_sequences(x_test, maxlen=maxlen)
+
+    model = Sequential();
+    model.add(Embedding(10000, 8, input_length=maxlen))
+
+    model.add(Flatten())
+
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+    model.summary()
+
+    history = model.fit(x_train, y_train, epochs=10,
+    batch_size=32, validation_split=0.2)
 
     
